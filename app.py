85119ed984632532a8a2e210236a6bb2b81fd2f7
Aayush Jain <aayushj2904@gmail.com>
2025-07-09 19:25:14 +0530

    Updated homepage message

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ AniToolkit is running! Deployed successfully."
+    return "⚙️ AniToolkit is loading… check back soon!"
 
+if __name__ == "__main__":
+    app.run(debug=True)
